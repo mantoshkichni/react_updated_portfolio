37d9cec1fbc92c12973176f10620c6b6ca493411
mantoshkichni <73746406+mantoshkichni@users.noreply.github.com>
2022-12-20 18:12:01 +0530

    deployment deploymenet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Aboutme } from "../component/Aboutme";
-import { Contactme } from "../component/Contactme";
-import { Experience } from "../component/Experience";
-import { Home } from "../component/Home";
-import { Navbar } from "../component/Navbar";
-import { Work } from "../component/Work";
+import { Aboutme } from "./component/Aboutme";
+import { Contactme } from "./component/Contactme";
+import { Experience } from "./component/Experience";
+import { Home } from "./component/Home";
+import { Navbar } from "./component/Navbar";
+import { Work } from "./component/Work";
 import "./styles.css";
 
 export default function App() {
